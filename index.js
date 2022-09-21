be91756daa12dc181a2848a9b1db13012dcd6510
Alfred <somethingfly@somethingfly.com>
2022-09-21 15:32:51 -0700

    commit tree 8c087425ff6418573a2bf49c36712cee1cdf09df

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const stringLength = word.length;
+  const halfLength = Math.ceil(stringLength/2);
+  const startHalf = word.substring(0,halfLength);
+  const endHalf = word.substring(stringLength - halfLength);
+  let endHalfBackward = "";
+  for (element in endHalf) {
+    endHalfBackward = endHalf[element] + endHalfBackward;
+  };
+  return (startHalf == endHalfBackward)
 }
 
 /* 
   Add your pseudocode here
+
+  I'm going to take my working code from last time
+  And then put it here
+  Really don't get the point of this, am I supposed to copy the solution on the page?
+  Don't understand and I don't think it was clear
+
 */
 
 /*
   Add written explanation of your solution here
+
+  it works last time, so it'll pcrobably work again
+
 */
 
 // You can run `node index.js` to view these console logs
